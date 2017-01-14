2ad768c4871d1efd0c0d3a238730666c8ee91133
Hoang Pham <huyhoang8a5@gmail.com>
2017-01-14 18:03:47 +0800

    Finish refactoring and add content

--- change/js/lang.js
+++ change/js/lang.js
@@ -4,7 +4,21 @@ Lang.prototype.pack.en = {
         "Nhân vật": "Character",
         "Thư viện ảnh": "Gallery",
         "Mua game": "Buy game",
-        "story-content": "CHANGE! will bring you to the world of a Vietnamese high school girl - Tran Hong Mai (name changeable), who was just an ordinary girl living a quiet, boring life... until her seventeenth birthday party.Mai's body suddenly swapped with her childhood friend - Nguyen Duc Hiep. Without knowing the cause of this bizarre event, they are forced to accept their strange circumstances and continue to live as nornally as possible, while hoping for a way to change back.",
+        "Khác": "Others",
+        "PV mới": "New PV",
+        "Demo tiếng Anh": "English Demo",
+        "Demo tiếng Việt": "Vietnamese Demo",
+        "story-content": "CHANGE! will bring you to the world of a Vietnamese high school girl - Tran Hong Mai (name changeable), who was just an ordinary girl living a quiet, boring life... until her seventeenth birthday party. Mai's body suddenly swapped with her childhood friend - Nguyen Duc Hiep. Without knowing the cause of this bizarre event, they are forced to accept their strange circumstances and continue to live as nornally as possible, while hoping for a way to change back.",
+        "mai-tag": "Protagonist (Name changeable)",
+        "mai-content": "Mai's life is like a gloomy book. Her parents are all doctors - which keeps them away from home most of the time. As a result, she’s used to living alone. At the age of 17, just like many other girls, Mai is constantly worrying about her grades and relationships with her friends and family. And of course, her inner wish is to have a nice boyfriend. Unfortunately, Mai is kind of a shy girl and cannot find the courage to talk to her crush. Despite this, she has incredibly strong feelings for her friends, and would stand against the world for them.",
+        "hiep-tag": "Mai's neighbour and childhood friend.",
+        "hiep-content": "His parents are separated and are so busy with their careers that they rarely visit himand he often at home alone. Two kids in the same shoes, gradually grew close to each other. Hiep was once a kind and warm-hearted person. As the two grew a bit older and entered high school, Hiep changed completely. He has become a meanie that loves to make fun of Mai whenever he has the chance. There is little left of the quiet and caring boy that Mai used to knew. Or is there? Will you find out what is hidden behind Hiep’s grumpy attitude?",
+        "tuan-tag": "Highschool hotboy",
+        "tuan-content": "Tuan is famous in high school with his chiselled figure and also is the ideal boyfriend in many girls’ dream. Friendly as he might be, most of his friends are deliquents, which makes Mai unable to have the guts to make friend with him. After the body swapping, Mai realized something strange in his attitude towards her and Hiep, what is the reason behind his actions?",
+        "an-tag": "Big brother",
+        "an-content": "An is Na's cousin. In contrast to the hyperactive Na, An is calm, gentle and sweet. He acts like a big brother and loves to take care of Na and Mai. This 20 year-old guy is often mistaken for a girl or a secondary school student because of his baby face. If you choose to engage in An’s route during the game, get ready to be drown in a flood of his sweet and lovely actions!",
+        "na-tag": "Mai's BFF",
+        "na-content": "She is the only daughter of Vu family - the owner of a large corporation. Despite coming from an incredibly rich background, Ngoc Anh is not even slightly cocky. She is friendly, humorous and always full of energy. Everyone loves her and calls her by a sweet nickname \"Na\". She may act so weird and unreasonable at the start, but as you process throughout the game, you may find out how sweet and thoughtful Na can actually be."
     },
     "regex": []
 };
